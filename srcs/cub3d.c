736a03f38b71172415f7b496af4f8644eacdd380
andrexandre <106836082+andrexandre@users.noreply.github.com>
2024-02-02 17:22:01 +0000

    Added all

--- srcs/cub3d.c
+++ srcs/cub3d.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cub3d.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jealves- <jealves-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: analexan <analexan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/31 19:31:51 by jealves-          #+#    #+#             */
-/*   Updated: 2024/01/31 19:31:55 by jealves-         ###   ########.fr       */
+/*   Updated: 2024/02/02 14:55:10 by analexan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,11 +24,29 @@ int	cub3d(char *map_path)
 	build(map_path);
 	return (1);
 }
+#define mapWidth 7
+#define mapHeight 7
+#define screenWidth 640
+#define screenHeight 480
 
-int	main(int ac, char **av)
+int worldMap[mapWidth][mapHeight] =
 {
-	if (ac == 2)
-		return (cub3d(av[1]));
-	else
-		error_msg("Invalid Number of Arguments");
+  {1,1,1,1,1,1,1},
+  {1,0,0,0,0,0,1},
+  {1,0,0,0,0,0,1},
+  {1,0,0,0,0,0,1},
+  {1,0,0,0,0,0,1},
+  {1,0,0,0,0,0,1},
+  {1,1,1,1,1,1,1}
+};
+
+// int	main(int ac, char **av)
+int	main(void)
+{
+	
+	return (0);
+	// if (ac == 2)
+	// 	return (cub3d(av[1]));
+	// else
+	// 	error_msg("Invalid Number of Arguments");
 }
